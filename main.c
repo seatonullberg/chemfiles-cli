a88e6411b68787f63df27d3c60b9ed24521ad15e
Seaton Ullberg <seatonullberg@gmail.com>
2021-10-18 10:51:39 -0400

    Major functionality works now

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
+#include <chemfiles/capi/trajectory.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "argparse/argparse.h"
+#include "chemfiles.h"
 
 /* program usage string */
 static const char *const usage[] = {
@@ -18,6 +20,9 @@ static const char *const epilogue =
     "\nAuthor: Seaton Ullberg <seatonullberg@gmail.com>\nVersion: 0.1.0";
 
 int main(int argc, const char **argv) {
+  /* iteration counter */
+  int i;
+
   /* initialize positional arguments */
   const char *src = NULL;
   const char *dst = NULL;
@@ -44,10 +49,40 @@ int main(int argc, const char **argv) {
   src = argv[0];
   dst = argv[1];
 
-  printf("src = %s\n", src);
-  printf("dst = %s\n", dst);
-  printf("in-fmt = %s\n", in_fmt);
-  printf("out-fmt = %s\n", out_fmt);
+  
+  /* read a trajectory from the input file */
+  CHFL_TRAJECTORY* in_trajectory = NULL;
+  if (in_fmt == NULL)
+    in_trajectory = chfl_trajectory_open(src, 'r');
+  else
+   in_trajectory = chfl_trajectory_with_format(src, 'r', in_fmt);
+
+  /* determine the number of frames (steps) in the input trajectory */
+  uint64_t nframes = 0;
+  chfl_trajectory_nsteps(in_trajectory, &nframes);
+
+
+  /* initialize the necessary number of frames */
+  CHFL_FRAME* frames[nframes];
+
+  /* read each frame from the input trajectory */
+  for (i = 0; i < nframes; i++) {
+      CHFL_FRAME* frame = chfl_frame();
+      chfl_trajectory_read(in_trajectory, frame);
+      frames[i] = frame;
+  }
+
+  /* create an empty output trajectory with the desired format */
+  CHFL_TRAJECTORY* out_trajectory = NULL;
+  if (out_fmt == NULL)
+    out_trajectory = chfl_trajectory_open(dst, 'a');
+  else
+    out_trajectory = chfl_trajectory_with_format(dst, 'a', out_fmt);
+
+  /* write each frame from the input trajectory into the output trajectory */
+  for (i = 0; i < nframes; i++) {
+      chfl_trajectory_write(out_trajectory, frames[i]);
+  }
 
   return 0;
 }
